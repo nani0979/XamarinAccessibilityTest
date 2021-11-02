f6610eca4524f772aaa00e42f6863efc9b8eb82f
nani0979 <52694220+nani0979@users.noreply.github.com>
2021-11-02 17:35:17 +0800

    to temp uncomplete code.

--- AccessibilityTesting/AccessibilityTesting/Label/FirstLabelPage.xaml.cs
+++ AccessibilityTesting/AccessibilityTesting/Label/FirstLabelPage.xaml.cs
@@ -16,5 +16,10 @@ namespace AccessibilityTesting.Label
         {
             InitializeComponent();
         }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondLabelPage());
+        }
     }
 }
