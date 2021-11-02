397881f9f33c5ab5ecba8eaaddb1d845122e8766
nani0979 <52694220+nani0979@users.noreply.github.com>
2021-11-02 23:43:33 +0800

    to add listview test code.

--- AccessibilityTesting/AccessibilityTesting/ListView/FirstListViewPage.xaml.cs
+++ AccessibilityTesting/AccessibilityTesting/ListView/FirstListViewPage.xaml.cs
@@ -12,9 +12,22 @@ namespace AccessibilityTesting.ListView
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FirstListViewPage : ContentPage
     {
+        public List<string> _items;
         public FirstListViewPage()
         {
             InitializeComponent();
+            _items = new List<string>();
+            _items.Add("項目一");
+            _items.Add("項目二");
+            lv1.ItemsSource = _items;
+            lv2.ItemsSource = _items;
+            lv3.ItemsSource = _items;
+            lv4.ItemsSource = _items;
+            lv5.ItemsSource = _items;
+            lv6.ItemsSource = _items;
+            lv7.ItemsSource = _items;
+            lv8.ItemsSource = _items;
+            lv1.ItemsSource = _items;
         }
     }
 }
