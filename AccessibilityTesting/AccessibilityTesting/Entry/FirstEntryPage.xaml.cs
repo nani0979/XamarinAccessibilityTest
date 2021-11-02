623c51eee3801a6f3236392ad5355aafdeb42f6e
nani0979 <52694220+nani0979@users.noreply.github.com>
2021-11-02 23:36:23 +0800

    to update test Accessibility codes.

--- AccessibilityTesting/AccessibilityTesting/Entry/FirstEntryPage.xaml.cs
+++ AccessibilityTesting/AccessibilityTesting/Entry/FirstEntryPage.xaml.cs
@@ -16,5 +16,10 @@ namespace AccessibilityTesting.Entry
         {
             InitializeComponent();
         }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondEntryPage());
+        }
     }
 }
