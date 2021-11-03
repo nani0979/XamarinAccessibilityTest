4eda9190251811d6261213dae4a8225284b94804
nani0979 <52694220+nani0979@users.noreply.github.com>
2021-11-03 17:39:15 +0800

    to add test code for element changed.

--- AccessibilityTesting/AccessibilityTesting/Label/SecondLabelPage.xaml.cs
+++ AccessibilityTesting/AccessibilityTesting/Label/SecondLabelPage.xaml.cs
@@ -12,9 +12,35 @@ namespace AccessibilityTesting.Label
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SecondLabelPage : ContentPage
     {
+        private string format = "";
+        private bool _isStart;
+        private int currentIndex = 0;
         public SecondLabelPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!_isStart)
+                SetTimer();
+        }
+
+        private void SetTimer()
+        {
+            _isStart = true;
+            Device.StartTimer(TimeSpan.FromSeconds(10), () =>
+            {
+                mLabel.Text = string.Format("Current index is : {0}", currentIndex);
+                currentIndex++;
+                return _isStart;
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _isStart = false;
         }
     }
 }
