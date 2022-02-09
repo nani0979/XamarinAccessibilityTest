17c4822ce8a6b493c4c859f1a0a2d495796650bc
nani0979 <52694220+nani0979@users.noreply.github.com>
2022-02-09 14:04:03 +0800

    to correct terms.

--- AccessibilityTesting/AccessibilityTesting/DeepLinkTest/Navigation.cs
+++ AccessibilityTesting/AccessibilityTesting/DeepLinkTest/Navigation.cs
@@ -6,7 +6,7 @@ namespace AccessibilityTesting.DeepLinkTest
 {
     static public class Navigation
     {
-        const string StartNavigationLink = "seeingIGo://Notify_SIG_of_Navigation?location_latitude={0}&location_longitude={1}";
+        const string StartNavigationLink = "SeeingIGo://Notify_SIG_of_Navigation?location_latitude={0}&location_longitude={1}";
         static public Uri getNavigationString(string latitude, string longitude)
         {
             return new Uri(string.Format(StartNavigationLink, longitude, latitude));
--- AccessibilityTesting/AccessibilityTesting/DeepLinkTest/StartTrip.cs
+++ AccessibilityTesting/AccessibilityTesting/DeepLinkTest/StartTrip.cs
@@ -6,7 +6,7 @@ namespace AccessibilityTesting.DeepLinkTest
 {
     static public class StartTrip
     {
-        const string StartTripnLink = "seeingIGo://Notify_SIG_of_Start_Trip?rider_name={0}&&rider_id={1}";
+        const string StartTripnLink = "SeeingIGo://Notify_SIG_of_Start_Trip?rider_name={0}&&rider_id={1}";
         static public Uri getStartTripLink(string RiderID, string RiderName)
         {
             return new Uri(string.Format(StartTripnLink, RiderName, RiderID));
